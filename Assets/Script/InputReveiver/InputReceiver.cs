35d1bc334423971aa3c19734fdf0f3bcf0a26070
polu_kun <polu635241@gmail.com>
2020-01-25 22:33:25 +0800

    新增硬體輸入控制器 透過抽象型別方便硬體抽換

---
+++ Assets/Script/InputReveiver/InputReceiver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kun.HardwareInput
+{
+	/// <summary>
+	/// 方便後續抽換輸入模組
+	/// </summary>
+	public abstract class InputReceiver
+	{
+		public abstract bool Jump ();
+
+		public abstract bool Right ();
+
+		public abstract bool Left ();
+
+		public abstract bool Up ();
+
+		public abstract bool Down();
+
+		public abstract bool Rush ();
+
+		public abstract bool TriggerDown (out Vector3 triggerPoint);
+	}
+}
---
+++ Assets/Script/InputReveiver/KeyboardMouseInputReceiver.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kun.HardwareInput
+{
+	public class KeyboardMouseInputReceiver:InputReceiver
+	{		
+		public override bool Jump ()
+		{
+			return Input.GetKeyDown (KeyCode.Space);
+		}
+
+		public override bool Right ()
+		{
+			return Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow);
+		}
+
+		public override bool Left ()
+		{
+			return Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow);
+		}
+
+		public override bool Up ()
+		{
+			return (Input.GetKey (KeyCode.UpArrow)||Input.GetKey (KeyCode.W));
+		}
+
+		public override bool Down ()
+		{
+			return (Input.GetKey (KeyCode.DownArrow)||Input.GetKey (KeyCode.S));
+		}
+
+		public override bool Rush ()
+		{
+			return Input.GetKey (KeyCode.LeftShift);
+		}
+
+		public override bool TriggerDown (out Vector3 triggerPoint)
+		{
+			if (Input.GetMouseButtonDown (0)) 
+			{
+				triggerPoint = Input.mousePosition;
+				return true;
+			}
+			else
+			{
+				triggerPoint = Vector3.zero;
+				return false;
+			}
+		}
+	}
+}
