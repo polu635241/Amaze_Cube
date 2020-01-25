51f0987c226f8a06d8e136abd09f1ff5d9f7e1e9
polu_kun <polu635241@gmail.com>
2020-01-26 01:00:44 +0800

    建構基礎的方塊控制器 以及對應的狀態機系統

---
+++ Assets/Script/Controller/CubeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kun.Tool;
+using Kun.HardwareInput;
+
+namespace Kun.Controller
+{
+	public class CubeController : GenericEntityController {
+
+		InputReceiver inputReceiver;
+
+		public InputReceiver InputReceiver
+		{
+			get
+			{
+				return inputReceiver;
+			}
+		}
+
+		[SerializeField][ReadOnly]
+		CubeFlowController cubeFlowController;
+
+		protected override void Awake ()
+		{
+			base.Awake ();
+			InitController ();
+		}
+
+		void Update ()
+		{
+			float deltaTime = Time.deltaTime;
+
+			cubeFlowController.Stay (deltaTime);
+		}
+
+
+		void InitController ()
+		{
+			inputReceiver = new KeyboardMouseInputReceiver ();
+			cubeFlowController = new CubeFlowController (this);
+		}
+	}
+}
---
+++ Assets/Script/Controller/Cube_Fsm/States/CubeStandbyState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kun.HardwareInput;
+using Kun.Tool;
+
+namespace Kun.Controller
+{
+	public class CubeStandbyState : CubeFlowState {
+
+		public CubeStandbyState (CubeController cube_Controller, CubeFlowController cubeFlowController) : base (cube_Controller, cubeFlowController)
+		{
+
+		}
+
+		public override void Enter (CubeFlowState prevState)
+		{
+			base.Enter (prevState);
+		}
+
+		public override CubeFlowState Stay (float deltaTime)
+		{
+			return null;
+		}
+	}
+}
