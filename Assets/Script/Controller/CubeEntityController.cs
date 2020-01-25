f93bb78ce6e2c7fa6c87408397104fe572dc4ca0
polu_kun <polu635241@gmail.com>
2020-01-26 03:35:05 +0800

    新增方塊緩存器 新增方塊實體控制器 並提供API使外部可以 設定群體旋轉

---
+++ Assets/Script/Controller/CubeEntityController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kun.Tool;
+using Kun.Data;
+using Kun.HardwareInput;
+
+namespace Kun.Controller
+{
+	[Serializable]
+	public class CubeEntityController
+	{
+		public CubeEntityController (Transform centerPoint, List<Transform> subCubes, CubeEntitySetting cubeEntitySetting)
+		{
+			this.centerPoint = centerPoint;
+
+			currentWholeRot = centerPoint.rotation;
+			currentWholeEuler = centerPoint.eulerAngles;
+
+			cubeCacheDatas = new List<CubeCacheData> ();
+			subCubes.ForEach (subCube=>
+				{
+					cubeCacheDatas.Add (new CubeCacheData (centerPoint, subCube));
+				});
+
+			this.cubeEntitySetting = cubeEntitySetting;
+		}
+		
+		[SerializeField]
+		Transform centerPoint;
+
+		[SerializeField][ReadOnly]
+		List<CubeCacheData> cubeCacheDatas;
+
+		[SerializeField][ReadOnly]
+		CubeEntitySetting cubeEntitySetting;
+
+		[SerializeField][ReadOnly]
+		Quaternion currentWholeRot;
+
+		[SerializeField][ReadOnly]
+		Vector3 currentWholeEuler;
+
+		public void RotateWhole (Vector3 deltaEuler, float deltaTime)
+		{
+			Vector3 processDeltaEuler = deltaEuler * cubeEntitySetting.RotateSpeed * deltaTime;
+
+			Quaternion deltaRot = Quaternion.Euler (processDeltaEuler);
+
+			SetWholeRotCache (deltaRot);
+			ProcessCubeCacheDatas ();
+		}
+
+		void SetWholeRotCache(Quaternion deltaRot)
+		{
+			currentWholeRot = deltaRot * currentWholeRot;
+			currentWholeEuler = currentWholeRot.eulerAngles;
+		}
+
+		void ProcessCubeCacheDatas ()
+		{
+			cubeCacheDatas.ForEach (cubeCacheData=>
+				{
+					Transform bindTransform = cubeCacheData.BindTransform;
+
+					//原始的相對座標當作旋轉矩 往新的方向轉
+					Vector3 newPos = centerPoint.position + currentWholeRot * cubeCacheData.OriginRelativelyPos;
+
+					bindTransform.rotation = currentWholeRot;
+					bindTransform.position = newPos;
+				});
+		}
+	}
+}
