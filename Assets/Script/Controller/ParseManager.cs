1f50518073e0b525545a4584576773d48f5e238c
polu_kun <polu635241@gmail.com>
2020-01-26 02:13:00 +0800

    新增基礎的設定檔以及解析管理器

---
+++ Assets/Script/Controller/ParseManager.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kun.Tool;
+using Kun.Data;
+
+namespace Kun.Manager
+{
+	public class ParseManager 
+	{
+		public void ParseSettings()
+		{
+			cubeSetting = JsonLoader<CubeSetting> ();
+		}
+		
+		[SerializeField][ReadOnly]
+		CubeSetting cubeSetting;
+
+		public CubeSetting CubeSetting
+		{
+			get
+			{
+				return cubeSetting;
+			}
+		}
+
+        /// <summary>
+        /// 若檔名與泛型名一樣 則直接套用
+        /// </summary>
+        /// <returns>The loader.</returns>
+        /// <param name="dataName">Data name.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public static T JsonLoader<T>(string dataName = "")
+        {
+            if (dataName == "")
+            {
+                Type type = typeof(T);
+
+                dataName = type.Name;
+            }
+
+            var allLines = "";
+
+            DirectoryInfo settingdirInfo = new DirectoryInfo(SettingFolderPath);
+            string filePath = Path.Combine(settingdirInfo.FullName, dataName + ".json");
+
+			if (File.Exists(filePath))
+			{
+				using (StreamReader sr = new StreamReader(filePath))
+				{
+					allLines = sr.ReadToEnd();
+				}
+
+				if (string.IsNullOrEmpty(allLines))
+				{
+					Debug.LogErrorFormat("無法讀取設定! 請檢查{0}是否存在!", dataName);
+				}
+			}
+			else
+			{
+				throw new Exception (string.Format ("路徑不存在{0}", filePath));
+			}
+
+            T process = JsonUtility.FromJson<T>(allLines);
+
+            return process;
+        }
+
+        public const string SettingPath = "GameSetting";
+
+        static string settingfolderPath = "";
+
+        public static string SettingFolderPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(settingfolderPath))
+                {
+                    DirectoryInfo dirInfo = new DirectoryInfo(Application.dataPath);
+                    settingfolderPath = Path.Combine(dirInfo.Parent.FullName, SettingPath);
+                }
+
+                return settingfolderPath;
+            }
+        }
+    }
+}
---
+++ Assets/Script/Controller/Setting/CubeEntitySetting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kun.Tool;
+
+namespace Kun.Data
+{
+	[Serializable]
+	public class CubeEntitySetting 
+	{
+		[SerializeField][Header("旋轉速度")][ReadOnly]
+		float rotateSpeed;
+
+		/// <summary>
+		/// 旋轉速度
+		/// </summary>
+		/// <value>The rotate speed.</value>
+		public float RotateSpeed
+		{
+			get
+			{
+				return rotateSpeed;
+			}
+
+			set
+			{
+				rotateSpeed = value;
+			}
+		}
+	}
+}
