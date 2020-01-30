5e31a0b927c5096688c2f8d1dc5bac0205d0c545
DESKTOP-33FM5VK\polu6 <polu635241@gmail.com>
2020-01-30 21:27:14 +0800

    新增綁定腳本 修改場景物件的綁定模式

---
+++ Assets/Script/Data/CubeBindDataGroup.cs
@@ -0,0 +1,96 @@
+﻿//#define DrawDebugLine
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kun.Tool;
+
+namespace Kun.Data
+{
+	public class CubeBindDataGroup : MonoBehaviour
+	{
+		public int GroupIndex
+		{
+			get
+			{
+				return groupIndex;
+			}
+		}
+
+		[SerializeField]
+		int groupIndex;
+
+		public List<CubeBindDataRow> HorizontalRows
+		{
+			get
+			{
+				return horizontalRows;
+			}
+
+			set
+			{
+				horizontalRows = value;
+			}
+		}
+
+		[SerializeField]
+		List<CubeBindDataRow> horizontalRows = new List<CubeBindDataRow> ();
+
+		public List<CubeBindDataRow> VerticalRows
+		{
+			get
+			{
+				return verticalRows;
+			}
+
+			set
+			{
+				verticalRows = value;
+			}
+		}
+
+		[SerializeField]
+		List<CubeBindDataRow> verticalRows = new List<CubeBindDataRow> ();
+
+		#if UNITY_EDITOR && DrawDebugLine
+
+		const float biggestWidth = 50f;
+
+		void OnDrawGizmos ()
+		{
+			DrawCubeBindDataRow (horizontalRows, Color.blue);
+			DrawCubeBindDataRow (verticalRows, Color.red);
+		}
+
+		void DrawCubeBindDataRow (List<CubeBindDataRow> rows, Color drawColor)
+		{
+			if (rows.Count > 0)
+			{
+				rows.ForEach (row=>
+					{
+						int rowItemCount = row.CubeEntitys.Count;
+
+						if(rowItemCount > 0)
+						{
+							float rangeDeltaWidth = biggestWidth / rowItemCount;
+
+							//最後一個點沒有下一個點了
+							for (int rangeIndex = 0; rangeIndex < row.CubeEntitys.Count -1 ; rangeIndex++)
+							{
+								float rangeWidth = biggestWidth - (rangeDeltaWidth * rangeIndex);
+
+								Transform currentPoint = row.CubeEntitys[rangeIndex];
+								Transform nextPoint = row.CubeEntitys[rangeIndex +1];
+								Color originColor = Gizmos.color;
+								Gizmos.color = drawColor;
+								Gizmos.DrawLine(currentPoint.position, nextPoint.position);
+								Gizmos.color = originColor;
+							}
+						}
+					});
+			}
+		}
+
+		#endif
+	}
+}
