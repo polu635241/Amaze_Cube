5304ec60d86477d499e94bbd9c4561373ace257c
DESKTOP-33FM5VK\polu6 <polu635241@gmail.com>
2020-02-10 01:28:26 +0800

    新增旋轉同步功能

---
+++ Assets/Script/Debug/DebugBoxSync.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kun.Tool;
+using Kun.Data;
+using Kun.HardwareInput;
+
+namespace Kun.Controller
+{
+	public class DebugBoxSync : GenericEntityController 
+	{
+		[SerializeField]
+		CubeController cubeController;
+
+		void Awake ()
+		{
+			base.Awake ();
+		}
+
+		void Update ()
+		{
+			if (cubeController != null) 
+			{
+				CubeEntityController cubeEntityController = cubeController.CubeEntityController;
+				
+				if (cubeEntityController != null) 
+				{
+					m_Transform.rotation = cubeEntityController.CurrentWholeRot;
+				}	
+			}
+		}
+	}
+}
