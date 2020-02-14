5944d0e21ddd09f0182c3b08b0fdc010a49794bf
DESKTOP-33FM5VK\polu6 <polu635241@gmail.com>
2020-02-15 00:57:42 +0800

    新增Tag常數

---
+++ Assets/Script/Consts/Tags.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kun.Tool;
+
+namespace Kun.Data
+{
+	public static class Tags 
+	{
+		public const string SceneRefBinder = "SceneRefBinder";
+	}
+}
