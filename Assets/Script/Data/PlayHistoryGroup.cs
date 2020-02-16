192c3daa8a7d96d6620d50fbf5771b3d073a0f7e
DESKTOP-33FM5VK\polu6 <polu635241@gmail.com>
2020-02-16 09:28:16 +0800

    新增資料型態緩存玩家操作方塊的歷程記錄

---
+++ Assets/Script/Data/PlayHistoryGroup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Timers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kun.Tool;
+
+namespace Kun.Data
+{
+	[Serializable]
+	public class PlayHistoryGroup
+	{
+		public PlayHistoryGroup (DateTime startTime)
+		{
+			this.startTime = startTime;
+			this.playHistorys = new List<PlayHistory> ();
+		}
+		
+		public DateTime StartTime
+		{
+			get
+			{
+				return startTime;
+			}
+		}
+		
+		DateTime startTime;
+		
+		public List<PlayHistory> PlayHistorys
+		{
+			get
+			{
+				return playHistorys;
+			}
+		}
+
+		[SerializeField][ReadOnly]
+		List<PlayHistory> playHistorys = new List<PlayHistory> ();
+
+		/// <summary>
+		/// 因為反序列化的時候 不能判斷抽象型別 所以使用變數緩存再透過內部的enum判斷回傳
+		/// </summary>
+		/// <param name="time">Time.</param>
+		/// <param name="rowRotateAxis">Row rotate axis.</param>
+		/// <param name="isPositive">If set to <c>true</c> is positive.</param>
+		public void AddPlayRowRotateHistory (float time, int rowIndex, RowRotateAxis rowRotateAxis, bool isPositive)
+		{
+			PlayHistory rowRotateHistory = PlayHistory.GetPlayRowRotateHistory (time, rowIndex, rowRotateAxis, isPositive);
+			this.playHistorys.Add (rowRotateHistory);
+		}
+
+		/// <summary>
+		//  因為反序列化的時候 不能判斷抽象型別 所以使用變數緩存再透過內部的enum判斷回傳
+		/// </summary>
+		/// <returns>The play whole rotate history.</returns>
+		/// <param name="time">Time.</param>
+		/// <param name="rowRotateAxis">Row rotate axis.</param>
+		/// <param name="isPositive">If set to <c>true</c> is positive.</param>
+		public void AddPlayWholeRotateHistory (float time, Vector2 deltaPos)
+		{
+			PlayHistory wholeRotateHistory = PlayHistory.GetPlayWholeRotateHistory (time, deltaPos);
+			this.playHistorys.Add (wholeRotateHistory);
+		}
+	}
+}
--- Assets/Script/GameController/Game_Fsm/GameFlowData.cs
+++ Assets/Script/GameController/Game_Fsm/GameFlowData.cs
@@ -24,5 +24,39 @@ namespace Kun.Data
 		
 		[SerializeField][ReadOnly]
 		float flowTime;
+
+		/// <summary>
+		/// 因為反序列化的時候 不能判斷抽象型別 所以使用變數緩存再透過內部的enum判斷回傳
+		/// </summary>
+		/// <param name="rowRotateAxis">Row rotate axis.</param>
+		/// <param name="isPositive">If set to <c>true</c> is positive.</param>
+		public void AddPlayRowRotateHistory (int rowIndex, RowRotateAxis rowRotateAxis, bool isPositive)
+		{
+			this.playHistoryGroup.AddPlayRowRotateHistory (flowTime, rowIndex, rowRotateAxis, isPositive);
+		}
+
+		/// <summary>
+		/// 因為反序列化的時候 不能判斷抽象型別 所以使用變數緩存再透過內部的enum判斷回傳
+		/// </summary>
+		/// <param name="deltaPos">Delta position.</param>
+		public void AddPlayWholeRotateHistory (Vector2 deltaPos)
+		{
+			this.playHistoryGroup.AddPlayWholeRotateHistory (flowTime, deltaPos);
+		}
+
+		public PlayHistoryGroup PlayHistoryGroup
+		{
+			get
+			{
+				return playHistoryGroup;
+			}
+
+			set
+			{
+				playHistoryGroup = value;
+			}
+		}
+
+		PlayHistoryGroup playHistoryGroup;
 	}
 }
