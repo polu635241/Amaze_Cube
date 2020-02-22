426a5da7fb58e5d5ef603d571b3d108bc933e26b
DESKTOP-33FM5VK\polu6 <polu635241@gmail.com>
2020-02-23 02:06:17 +0800

    新增PosTweenController

---
+++ Assets/Script/Tool/Editor/PosTweenControllerEditor.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Kun.Tool
+{
+    [CustomEditor (typeof (PosTweenController))]
+    public class PosTweenControllerEditor : SerializedObjectEditor<PosTweenController>
+    {
+        float value;
+
+        Vector3 oldBenginPos;
+        Vector3 oldEndPos;
+
+        protected override void OnEnable ()
+        {
+            base.OnEnable ();
+            oldBenginPos = runtimeScript.BeginPos;
+            oldEndPos = runtimeScript.EndPos;
+        }
+
+        public override void OnInspectorGUI ()
+        {
+            base.OnInspectorGUI ();
+
+            bool currentFrameNeedRefresh = false;
+
+            if (oldBenginPos != runtimeScript.BeginPos)
+            {
+                currentFrameNeedRefresh = true;
+            }
+
+            if (oldEndPos != runtimeScript.EndPos)
+            {
+                currentFrameNeedRefresh = true;
+            }
+
+            if (currentFrameNeedRefresh)
+            {
+                ForceRefresh ();
+            }
+
+            float newValue = EditorGUILayout.Slider (value, 0, 1);
+
+            if (newValue != value)
+            {
+                value = newValue;
+                runtimeScript.SetValue (value);
+            }
+        }
+
+        void ForceRefresh ()
+        {
+            runtimeScript.Refresh ();
+        }
+    }
+}
---
+++ Assets/Script/Tool/PosTweenController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+namespace Kun.Tool
+{
+    public class PosTweenController : MonoBehaviour
+    {
+        [SerializeField]
+        Transform valueTransform;
+
+        public Vector3 BeginPos
+        {
+            get 
+            {
+                return beginPos;
+            }
+        }
+
+        [SerializeField]
+        Vector3 beginPos = Vector3.zero;
+
+        public Vector3 EndPos
+        {
+            get 
+            {
+                return endPos;
+            }
+        }
+
+        [SerializeField]
+        Vector3 endPos = Vector3.zero;
+
+        [SerializeField][ReadOnly][Header ("把差距值分成100等分")]
+        Vector3 proportionDeltaPos;
+
+        void Awake ()
+        {
+            Refresh ();
+        }
+
+        public void Refresh ()
+        {
+            proportionDeltaPos = (endPos - beginPos);
+
+            SetValue (0f);
+        }
+
+        public void SetValue (float value)
+        {
+            if (value < 0)
+            {
+                value = 0;
+            }
+
+            if (value > 1)
+            {
+                value = 1;
+            }
+
+            Vector3 progressPos = beginPos + value * proportionDeltaPos;
+
+            if (valueTransform != null)
+            {
+                valueTransform.localPosition = progressPos;
+            }
+        }
+    }
+}
